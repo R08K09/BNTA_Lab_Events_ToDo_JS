f0156a3beed86ef844f93c1014bb95516683b870
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-05-11 15:53:16 +0100

    finished 2 extension tasks

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,14 +1,18 @@
 const button = document.querySelector("#enter");
 const list = document.querySelector("#list");
 const todo = document.querySelector("#new-todo");
+const dateButton = document.getElementById("date-today");
+const completedList = document.querySelector("#completed-list");
 
 // add button event listener function
 button.addEventListener("click", () => {
     const newListItem = document.createElement("li");
     newListItem.innerText = todo.value;
     const deleteButton = createDeleteButton(newListItem);
+    const completeButton = createCompleteButton(newListItem);
     list.appendChild(newListItem);
     newListItem.appendChild(deleteButton);
+    newListItem.appendChild(completeButton);
 });
 
 // create delete button
@@ -21,15 +25,29 @@ const createDeleteButton = (newListItem) => {
     return deleteButton;
 }
 
-
 // create button for today's date
-
-const dateButton = document.getElementById("date-today");
-
 dateButton.addEventListener("click", () =>{
     const currentDate = new Date();
     const dateString = currentDate.toString();
     alert(dateString);
+});
 
-})
+const createCompleteButton = (newListItem) => {
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "Complete";
 
+    completeButton.addEventListener("click", (event) => {
+        itemIsCompleted(newListItem);
+    });
+    return completeButton;
+}
+
+const itemIsCompleted = (newListItem) => {
+    const completedItem = document.createElement("li");
+    newListItem.removeChild(newListItem.lastChild);
+    newListItem.removeChild(newListItem.lastChild);
+    completedItem.innerText = newListItem.innerText;
+    completedList.appendChild(completedItem);
+    completedItem.setAttribute("style", "color: grey");
+    list.removeChild(newListItem);
+}
